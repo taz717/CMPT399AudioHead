5685129efaee782b2c035557805decd9dc71e6e5
JoeFoote <93680046+JoeFoote@users.noreply.github.com>
2022-12-16 15:54:33 -0700

    Added documentation

--- radioDrive3.ino
+++ radioDrive3.ino
@@ -1,9 +1,5 @@
-/* This example drives each motor on the Zumo forward, then
-backward.  The yellow user LED is on when a motor should be
-running forward and off when a motor should be running backward.
-If a motor on your Zumo has been flipped, you can correct its
-direction by uncommenting the call to flipLeftMotor() or
-flipRightMotor() in the setup() function. */
+/* This program allows the robot to receive commands from radio and 
+   follow the user's directions.*/
 
 #include <Wire.h>
 #include <Zumo32U4.h>
@@ -22,6 +18,7 @@ char lib[100];
 int i = 0;
 int trace = NULL;
 
+/* Command to move the robot forward for 5 seconds */
 void forward(){
   //Serial.println("Turning on motors");
   motors.setSpeeds(motorSpeed, motorSpeed);
@@ -29,12 +26,14 @@ void forward(){
   //Serial.println("Turning off Motors");
   motors.setSpeeds(0,0);
   delay(2500);
+  /* Records the command if 'start' was said previously */
   if (trace == true){
     lib[i] = 'b';
     i += 1;
   }
 }
 
+/* Moves the robot backward for 5 seconds */
 void backward(){
   //Serial.println("Turning on motors");
   motors.setSpeeds(-motorSpeed, -motorSpeed);
@@ -42,6 +41,7 @@ void backward(){
   //Serial.println("Turning off Motors");
   motors.setSpeeds(0,0);
   delay(2500);
+  /* Records command if 'start' was read earlier */
   if (trace == true){
     lib[i] = 'f';
     i += 1;
@@ -49,31 +49,31 @@ void backward(){
 
 }
 
+/* Turns the robot right 90 degrees */
 void turnRight(){
   motors.setSpeeds(motorSpeed, -motorSpeed);
   delay(425);
   motors.setSpeeds(0,0);
+  /* Records command if 'start' was read earlier */
   if (trace == true){
     lib[i] = 'l';
     i += 1;
   }
 
 }
-
+/* Turns the robot left 90 degrees */
 void turnLeft(){
   motors.setSpeeds(-motorSpeed, motorSpeed);
   delay(375); // Bot turns left a little faster
   motors.setSpeeds(0,0);
+  /* Records command if 'start' was read earlier */
   if (trace == true){
     lib[i] = 'r';
     i += 1;
   }
 }
 
-void stop(){
-  //.trace = false;
-}
-
+/* Makes the robot go back to its original position */
 void reset(){
   if (trace == true){
     for(i; i>0; i--){
@@ -84,10 +84,11 @@ void reset(){
       if (lib[i] == 'p') stop;
     }
   }
+  /* Resets its parameters */
   trace = false;
   char lib[100];
 }
-
+/* Begins logging movement of robot */
 void start(){
   trace = true;
 }
@@ -120,15 +121,13 @@ void loop()
     
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
+      /* Functions used when testing receiver input */
       Serial.print("Message: ");
       Serial.println(buf);
-      
-      
       //if (buf[0] == 'f') Serial.println("YES!YES!YES!");
       //if (buf[0] == 'f') Serial.println("f: compared!");
       //if (buf[0] == 'b') Serial.println("b: compared!");
-    
-      
+      /* If chain to read commands for robot */
       if (buf[0] == 'f') forward();    
       if (buf[0] == 'b') backward();
       if (buf[0] == 'r') turnRight();
